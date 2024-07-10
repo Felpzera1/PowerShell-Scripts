3049b8fc7ad93328c0454b04954b58c4eaf15d7f
Felipe Manoel Bornhausen <90637879+Felpzera1@users.noreply.github.com>
2024-07-10 16:52:40 -0300

    Update AuditLog_Rem.ps1

--- AuditLog_Rem.ps1
+++ AuditLog_Rem.ps1
@@ -1,38 +1,27 @@
-﻿# Defina o diretório onde os logs serão salvos
 $logDirectory = "C:\AuditLogs"
 if (!(Test-Path -Path $logDirectory)) {
     New-Item -Path $logDirectory -ItemType Directory | Out-Null
 }
 
-# Defina a data atual
 $currentDate = Get-Date -Format "yyyy-MM-dd"
 
-# Defina o caminho do arquivo de log para remoções
 $logFilePath = "$logDirectory\auditlog-removidos-$currentDate.txt"
 
 try {
-    # Coletar eventos de remoção de grupo
     $events = Get-WinEvent -FilterHashtable @{
         LogName = 'Security';
         Id = 4729, 4733;
         StartTime = (Get-Date).AddDays(-1)
     } | ForEach-Object {
-        # Converter o evento para XML 
         $eventXml = [xml]$_.ToXml()
-
-        # Inicializar uma lista para armazenar todas as informações do evento
         $eventDetails = @()
 
-        # Iterar sobre todos os elementos no XML do evento
+       
         foreach ($dataNode in $eventXml.Event.EventData.Data) {
             $nodeName = $dataNode.Name
             $nodeValue = $dataNode.'#text'
-
-            # Formatar cada informação como uma linha de texto
             $eventDetails += "$nodeName $nodeValue"
         }
-
-        # Formatar a string com todas as informações do evento
         $eventDetailsString = @"
 Ação:                "Removido"
 Data e Hora:         $($_.TimeCreated)
@@ -43,11 +32,11 @@ Usuário:             $($eventXml.Event.EventData.Data | Where-Object { $_.Name 
 
 "@
 
-        # Escrever no arquivo de log
+        
         Add-Content -Path $logFilePath -Value $eventDetailsString
     }
 
-    # Verificar se há eventos coletados
+   
     if ($events.Count -eq 0) {
         Write-Host "Nenhum evento de remoção de grupo encontrado."
     } else {
@@ -57,7 +46,6 @@ Usuário:             $($eventXml.Event.EventData.Data | Where-Object { $_.Name 
     Write-Host "Erro ao coletar eventos de remoção de grupo: $_"
 }
 
-# Verificar se o arquivo de log foi criado para remoções
 if (!(Test-Path -Path $logFilePath)) {
     Write-Host "O arquivo de log para remoções não foi criado em $logFilePath."
 }
